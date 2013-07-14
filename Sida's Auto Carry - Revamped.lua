a8f7c8a198e478a5c07245a0378b145bbaa92bea
Sida <AnysAlmighty@gmail.com>
2013-07-14 12:18:37 +0100

    Replaced wrong hotkeys

--- Sida's Auto Carry - Revamped.lua
+++ Sida's Auto Carry - Revamped.lua
@@ -5,9 +5,9 @@
  
 --[[ Configuration ]]--
  
-local AutoCarryKey = 219
+local AutoCarryKey = 32
 local LastHitKey = string.byte("X")
-local MixedModeKey = string.byte("D")
+local MixedModeKey = string.byte("C")
 local LaneClearKey = string.byte("V")
  
 ------------ > Don't touch anything below here < --------------
