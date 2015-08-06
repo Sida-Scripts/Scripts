a53d061f58e34256528e03a185f7e11b842e2558
Sida <AnysAlmighty@gmail.com>
2015-08-06 20:10:06 +0100

    Revert "GetWebResult Temp Fix 5.5-HF"
    This reverts commit e33c9560cef51fe05a05c09790b5c18ad6fc06c2.

--- Misc/TempFixSAC (Patch 5.5-HF).lua
+++
@@ -1,5 +0,0 @@
-_G.GetWebResultb = _G.GetWebResult
-_G.GetWebResult = function(a,b)
-    return b:find("SidaBoL/") and "version=90.56" or GetWebResultb(a,b)
-end
-print("Loaded SAC Temp Fix...don't forget to remove later!")
