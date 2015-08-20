c02be1ebaeaa65317eb355a0abc1742d654be438
Sida <AnysAlmighty@gmail.com>
2015-08-20 17:05:43 +0100

    5.16 revision
    Updated for 5.16 bugsplat

--- Misc/TempFix.lua
+++ Misc/TempFix.lua
@@ -1,4 +1,12 @@
-_G.GetWebResult = function(a,b)
-  return "0"
+--[[
+    GetWebResult bugsplat temp fix (All Scripts)
+    Patch: 5.16
+    Note: Remove after problem is fixed in BoL!
+]]
+
+function OnLoad()
+    _G.GetWebResult = function(a,b)
+      return "0"
+    end
+    print("Loaded GetWebResult Temp Fix...don't forget to remove later!")
 end
-print("Loaded GetWebResult Temp Fix...don't forget to remove later!")
--- Misc/TempFixSAC.lua
+++ Misc/TempFixSAC.lua
@@ -1,5 +1,13 @@
-_G.GetWebResultb = _G.GetWebResult
-_G.GetWebResult = function(a,b)
-    return b:find("SidaBoL/") and "version=90.56" or GetWebResultb(a,b)
+--[[
+    GetWebResult bugsplat temp fix (SAC only)
+    Patch: 5.16
+    Note: Remove after problem is fixed in BoL!
+]]
+
+function OnLoad()
+    _G.GetWebResultb = _G.GetWebResult
+    _G.GetWebResult = function(a,b)
+        return b:find("SidaBoL/") and "version=90.56" or GetWebResultb(a,b)
+    end
+    print("Loaded SAC Temp Fix...don't forget to remove when BoL problem is fixed!")
 end
-print("Loaded SAC Temp Fix...don't forget to remove when BoL problem is fixed!")
