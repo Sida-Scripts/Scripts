454b8f0481aeb99ee2a11f60ce278976341cfcb3
Sida <AnysAlmighty@gmail.com>
2015-10-14 15:36:39 +0100

    temp fix garena

--- Misc/TempFixSAC.lua
+++ Misc/TempFixSAC.lua
@@ -7,7 +7,7 @@
 function OnLoad()
     _G.GetWebResultb = _G.GetWebResult
     _G.GetWebResult = function(a,b)
-        return b:find("SidaBoL/") and "version=90.61" or GetWebResultb(a,b)
+        return b:find("SidaBoL/") and "version=0.36" or GetWebResultb(a,b)
     end
     print("Loaded SAC Temp Fix...don't forget to remove when BoL problem is fixed!")
 end
