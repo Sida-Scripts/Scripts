112a043753d67ecb9d1f2b7ef24acde29526985e
Sida <AnysAlmighty+bol@gmail.com>
2016-01-22 23:38:56 +0000

    Fixed more text

--- Sida's Ward Jump.lua
+++ Sida's Ward Jump.lua
@@ -15,21 +15,21 @@
                   How To Use
     =========================================
 
-	Key Up Mode (default):
-	    1. Hold the hotkey (default 'T') to enter Jump Mode
-	    2. Release the hotkey to jump
+    Key Up Mode (default):
+        1. Hold the hotkey (default 'T') to enter Jump Mode
+        2. Release the hotkey to jump
 	    3. Left click to cancel while in Jump Mode
 
-	Key Up Mode:
-	    1. Press the hotkey (default 'T') to instantly jump
-	    2. If no viable target is available based on your settings, script will use a ward
+    Key Up Mode:
+        1. Press the hotkey (default 'T') to instantly jump
+        2. If no viable target is available based on your settings, script will use a ward
         3. If no wards are available, script will enter Jump Mode and instantly jump to the first available target unless you release the key or cancel with left click
 
-	When jumping, the script will:
-	    - Attempt to find a valid jump target based on your settings (supports any object in the game)
-	    - If no target is available, script will use the lowest priority ward item you have
+    When jumping, the script will:
+        - Attempt to find a valid jump target based on your settings (supports any object in the game)
+        - If no target is available, script will use the lowest priority ward item you have
 
-	Please Note:
+    Please Note:
         - Lee Sin cannot jump to enemies or enemy minions
         - Jarvan can only jump to his flag
 
